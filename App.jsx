aba406f3a799131e7cd370d3a0a69d0fd3c1e5a8
Moha <alisantos743@gmail.com>
2026-07-14 19:59:41 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import UserCard from "./UserCard";
+import Blog from "./Blog";
+
+const App = () => {
+  return (
+    <>
+      <UserCard />
+      <Blog />
+    </>
+  );
+};
+
+export default App;
